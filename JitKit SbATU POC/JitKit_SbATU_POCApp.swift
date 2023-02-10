4c6db7ce7d40255335c6d1a1d3417e0b4717fc83
swifticul <swifticul@icloud.com>
2023-02-10 22:48:22 +0100

    Initial Commit

---
+++ JitKit SbATU POC/JitKit_SbATU_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  JitKit_SbATU_POCApp.swift
+//  JitKit SbATU POC
+//
+//  Created by Tristan on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct JitKit_SbATU_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
