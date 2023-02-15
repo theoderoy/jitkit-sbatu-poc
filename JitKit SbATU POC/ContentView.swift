3e3d1cf51f5909d55959cca42f937cf3bff87744
swifticul <swifticul@icloud.com>
2023-02-15 21:00:00 +0100

    commit for the dayyy

--- JitKit SbATU POC/ContentView.swift
+++ JitKit SbATU POC/ContentView.swift
@@ -12,27 +12,40 @@ struct ContentView: View {
     @State var writtenappid = ("")
     
     var body: some View {
-        List {
-            Section {
-                VStack {
-                    Text("JitKit SbATU POC")
-                        .font(.largeTitle.weight(.medium))
-                    
-                    Spacer()
-                        .frame(width: 0, height: 20)
-                        .clipped()
-                    
-                    TextField("Enter App ID", text: $writtenappid)
-                        .frame(maxWidth: 300) .textFieldStyle(.roundedBorder)
-                    
-                    Spacer()
-                        .frame(width: 0, height: 20)
-                        .clipped()
-                    
-                    Link("Inject to specified App ID", destination: URL(string: "sidestore://sidejit-enable?url=\(writtenappid)")!).buttonStyle(ButtonFromInteractfulROFL()) .frame(maxWidth: 300)
-                    
+        NavigationView {
+            List {
+                
+                Section {
+                    VStack {
+                        Spacer()
+                            .frame(width: 0, height: 10)
+                            .clipped()
+                        
+                        TextField("Enter App ID", text: $writtenappid)
+                            .frame(maxWidth: 300) .textFieldStyle(.roundedBorder)
+                        
+                        Spacer()
+                            .frame(width: 0, height: 15)
+                            .clipped()
+                        
+                        Link("Inject to specified App ID", destination: URL(string: "sidestore://sidejit-enable?url=\(writtenappid)")!).buttonStyle(ButtonFromInteractfulROFL()) .frame(maxWidth: 300)
+                        
+                        Spacer()
+                            .frame(width: 0, height: 10)
+                            .clipped()
+                        
+                    }
                 }
-            }
+                
+                Section {
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("View App ID Slots")
+                    } .buttonStyle(ButtonFromInteractfulROFL())
+                } footer: {
+                    Text("- JitKit SbATU POC by swifticul \n- SideStore by SideTeam \n- SideJIT call URL scheme update by Spidy123222")
+                }
+            } .navigationTitle("JitKit SbATU POC")
+            
         }
     }
 }
